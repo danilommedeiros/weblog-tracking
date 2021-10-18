eb5b5c5665d48bf33c6d3b0edc2d5963ffd895ca
Danilo Medeiros <danilo.medeiros801@gmail.com>
2021-10-18 17:16:51 -0300

    elements modify

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Poppins_400Regular, Poppins_600SemiBold, useFonts} from '@expo-google-fonts/poppins';
+import {AppLoading} from 'expo';
+
 
 export default function App() {
+
+  const fonts = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if(!fonts){
+    return <AppLoading/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Boas vindas ao Weblog o mais novo rastreador de encomendas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF3755',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontFamily:"Poppins_600SemiBold",
+ 	  fontSize: 30,
+	  color: "white",
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	  marginTop: -500
+
+
+	
+
+    
+	
+	
+  }
 });
